e11b77cd4765bd51feda77c251aecc37c2e9fe82
Tommy Demers <tommy1416@hotmail.com>
2014-09-25 14:29:15 -0400

    Création IClientService

---
+++ GestResto/GestResto.Logic/Services/Definitions/IClientService.cs
@@ -0,0 +1,18 @@
+﻿using GestResto.Logic.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestResto.Logic.Services.Definitions
+{
+    interface IClientService
+    {
+        void Create(Client client);
+        IList<Client> RetrieveAll();
+        Client Retrieve(int pIdClient);
+        void Update(Client client);
+        void Delete(Client client);
+    }
+}
