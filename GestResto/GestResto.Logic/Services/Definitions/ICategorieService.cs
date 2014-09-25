647e7356e48e24e124ad4390ed58a79f35ea7821
Vincent Desrosiers <vinid223@gmail.com>
2014-09-25 14:23:59 -0400

    Ajout Dossier

---
+++ GestResto/GestResto.Logic/Services/Definitions/ICategorieService.cs
@@ -0,0 +1,18 @@
+﻿using GestResto.Logic.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestResto.Logic.Services.Defenitions
+{
+    public interface ICategorieService
+    {
+        void Create(Categorie categorie);
+        IList<Categorie> RetrieveAll();
+        Categorie Retrieve(int pIdCategorie);
+        void Update(Categorie categorie);
+        void Delete(Categorie categorie);
+    }
+}
