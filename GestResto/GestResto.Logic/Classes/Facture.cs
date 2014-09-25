48ccdf7c7ba52915c6c26098f31d9322ab3e3b99
Tommy Demers <tommy1416@hotmail.com>
2014-09-25 11:12:56 -0400

    Facture

--- GestResto/GestResto.Logic/Classes/Facture.cs
+++ GestResto/GestResto.Logic/Classes/Facture.cs
@@ -34,12 +34,24 @@ namespace GestResto.Logic.Classes
 
         #region Constructeurs
 
+        /// <summary>
+        /// Constructeur par défaut de la classe facture.
+        /// </summary>
         public Facture()
         {
             idFacture = null;
             DateCreation = new DateTime();
+            fPourcentageTaxe = 0;
+            ListeItems = new List<Item>();
         }
 
+        /// <summary>
+        /// Constructeur paramétré de la classe facture.
+        /// </summary>
+        /// <param name="pidFacture">id de la facture en paramètre.</param>
+        /// <param name="pDateCreation">La date de la création de la facture.</param>
+        /// <param name="pPourcentageTaxe">Le pourcentage de la facture lors de la date de création.</param>
+        /// <param name="pListeItems">Liste d'items de la facture.</param>
         public Facture(uint pidFacture, DateTime pDateCreation, float pPourcentageTaxe, List<Item> pListeItems)
         {
             idFacture = pidFacture;
