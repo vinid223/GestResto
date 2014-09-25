78db3106b0f0234dd8403e55f6a5e6647eebd891
Tommy Demers <tommy1416@hotmail.com>
2014-09-25 11:08:18 -0400

    Revert "Facture"
    This reverts commit 6120c6f98c7e3d293d01421f4813b4e51ba5686a.

--- GestResto/GestResto.Logic/Classes/Facture.cs
+++ GestResto/GestResto.Logic/Classes/Facture.cs
@@ -17,38 +17,10 @@ using System.Threading.Tasks;
 
 namespace GestResto.Logic.Classes
 {
-    /// <summary>
-    /// Classe facture qui permet d'enregistrer la liste d'items que le client a commandé.
-    /// On enregistre le pourcentage de la taxe au cas où elle a changé.
-    /// </summary>
-    public class Facture
-    {
-        #region Variables de la classe
-
-        private uint? idFacture;
-        private DateTime DateCreation;
-        private float fPourcentageTaxe;
-        private List<Item> ListeItems;
-
-        #endregion
-
-        #region Constructeurs
-
-        public Facture()
-        {
-            idFacture = null;
-            DateCreation = new DateTime();
-        }
-
-        public Facture(uint pidFacture, DateTime pDateCreation, float pPourcentageTaxe, List<Item> pListeItems)
-        {
-            idFacture = pidFacture;
-            DateCreation = pDateCreation;
-            fPourcentageTaxe = pPourcentageTaxe;
-            ListeItems = pListeItems;
-        }
-
-        #endregion
-
+    public class Facture {
+	    private uint idFacture ;
+	    private DATETIME DateCreation ;
+	    private float pourcentageTaxe ;
+	    private List<Item> ListeItems ;
     }
 }
