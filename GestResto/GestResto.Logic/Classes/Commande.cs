b16128f62cd4978e91e862f5ee692bb018873f17
Simon Turcotte <sim_turcotte@hotmail.com>
2014-09-25 10:50:31 -0400

    Partie Commande

--- GestResto/GestResto.Logic/Classes/Commande.cs
+++ GestResto/GestResto.Logic/Classes/Commande.cs
@@ -17,12 +17,54 @@ using System.Threading.Tasks;
 
 namespace GestResto.Logic.Classes
 {
-    public class Commande {
-	    private int idCommande ;
-	    private string Statut ;
-	    private DATETIME Debut ;
-	    private DATETIME Fin ;
-	    private List<Table> ListeTables ;
-	    private List<Facture> ListeFactures ;
+    /// <summary>
+    /// Classe contenant toutes les informations d'une commande
+    /// </summary>
+    public class Commande
+    {
+        #region Attributs
+
+	    private int? idCommande;
+	    private string Statut;
+	    private DateTime Debut;
+	    private DateTime? Fin;
+	    private List<Table> ListeTables;
+	    private List<Facture> ListeFactures;
+
+        #endregion
+
+        #region Constructeurs
+
+        /// <summary>
+        /// Constructeur par défaut de commande. Nouvelle commande
+        /// </summary>
+        public Commande()
+        {
+            idCommande = null;
+            Statut = "Active";
+            Debut = DateTime.Now;
+            Fin = null;
+            ListeTables = new List<Table>();
+            ListeFactures = new List<Facture>();   
+        }
+
+        /// <summary>
+        /// Constructeur paramétré de commande.
+        /// 
+        /// </summary>
+        /// <param name="pIdCommande">L'id de la commande</param>
+        /// <param name="pStatut">Le statut de la commande</param>
+        /// <param name="pListeTable">La liste des tables</param>
+        public Commande(int pIdCommande, string pStatut, DateTime pDebut, DateTime pFin, List<Table> pListeTable, List<Facture> pListeFactures)
+        {
+            idCommande = pIdCommande;
+            Statut = pStatut;
+            Debut = pDebut;
+            Fin = pFin;
+            ListeTables = pListeTable;
+            ListeFactures = pListeFactures;
+        }
+
+        #endregion
     }
 }
