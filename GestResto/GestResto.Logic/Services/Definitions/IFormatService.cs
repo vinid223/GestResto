8205b93d31bf4e3dd58d2e875c1df91f47290a1b
Tommy Demers <tommy1416@hotmail.com>
2014-09-25 14:42:39 -0400

    IFormatService

---
+++ GestResto/GestResto.Logic/Services/Definitions/IFormatService.cs
@@ -0,0 +1,18 @@
+﻿using GestResto.Logic.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestResto.Logic.Services.Definitions
+{
+    interface IClientService
+    {
+        void Create(Format format);
+        IList<Format> RetrieveAll();
+        Format Retrieve(int pIdFormat);
+        void Update(Format format);
+        void Delete(Format format);
+    }
+}
