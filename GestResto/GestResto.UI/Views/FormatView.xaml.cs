0d84f3dfde537ffef27089c181110db76cd9b11e
Tommy Demers <tommy1416@hotmail.com>
2014-09-30 11:08:12 -0400

    Format

--- GestResto/GestResto.UI/Views/FormatView.xaml.cs
+++ GestResto/GestResto.UI/Views/FormatView.xaml.cs
@@ -24,5 +24,10 @@ namespace GestResto.UI.Views
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
