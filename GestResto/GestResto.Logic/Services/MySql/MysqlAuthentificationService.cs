6bde73386e74de817e0a8217501bd77d819c6c3b
Vincent Desrosiers <vinid223@gmail.com>
2014-10-01 09:47:44 -0400

    Update

--- GestResto/GestResto.Logic/Services/MySql/MysqlAuthentificationService.cs
+++ GestResto/GestResto.Logic/Services/MySql/MysqlAuthentificationService.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GestResto.Logic.Services.Definitions;
+using MySql.Data.MySqlClient;
 
 namespace GestResto.Logic.Services.MySql
 {
     public class MysqlAuthentificationService : IAuthentificationService
     {
-        private MySqlConnexion connexion;
+        private MySqlConnection connexion;
     }
 }
