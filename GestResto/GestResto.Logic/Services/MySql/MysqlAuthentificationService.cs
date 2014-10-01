024f496ed67eb20153f403b90cd92f3f2bd6b534
Vincent Desrosiers <vinid223@gmail.com>
2014-10-01 09:51:03 -0400

    Update

--- GestResto/GestResto.Logic/Services/MySql/MysqlAuthentificationService.cs
+++ GestResto/GestResto.Logic/Services/MySql/MysqlAuthentificationService.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GestResto.Logic.Model.Entities;
 using GestResto.Logic.Services.Definitions;
-using MySql.Data.MySqlClient;
 
 namespace GestResto.Logic.Services.MySql
 {
     public class MysqlAuthentificationService : IAuthentificationService
     {
         private MySqlConnection connexion;
+
+        #region IAuthentificationService Membres
+
+        public Employe Retrieve(int pNumEmploye, string pMDP)
+        {
+            connexion = new MySqlConnection();
+
+            string query = "SELECT * FROM Employes WHERE Em"
+        }
+
+        #endregion
     }
 }
