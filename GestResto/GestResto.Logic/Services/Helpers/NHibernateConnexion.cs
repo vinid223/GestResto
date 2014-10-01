c6c467d2bae6e330ff5da569a4cd742ea6058aaa
Tommy Demers <tommy1416@hotmail.com>
2014-10-01 13:14:17 -0400

    Ajout Hibernate
    Ajout référence Hibernate dans .Logic, Création fichiers et dossiers

---
+++ GestResto/GestResto.Logic/Services/Helpers/NHibernateConnexion.cs
@@ -0,0 +1,35 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestResto.Logic.Services.Helpers
+{
+    public class NHibernateConnexion
+    {
+        private static ISessionFactory _sessionFactory;
+        private static readonly bool IS_FLUENT = true;
+
+        static NHibernateConnexion()
+        {
+            _sessionFactory = new Configuration().AddAssembly(typeof(NHibernateConnexion).Assembly).BuildSessionFactory();
+        }
+
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                return _sessionFactory;
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+
+        }
+    }
+}
