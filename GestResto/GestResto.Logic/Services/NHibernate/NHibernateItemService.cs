073ffa243334e30712ec1177e38eb813918fba34
Simon Turcotte <sim_turcotte@hotmail.com>
2014-10-02 10:49:01 -0400

    Ajout de toutes les classes NHibernate

---
+++ GestResto/GestResto.Logic/Services/NHibernate/NHibernateItemService.cs
@@ -0,0 +1,23 @@
+﻿using GestResto.Logic.Model.Args;
+using GestResto.Logic.Model.Entities;
+using GestResto.Logic.Services.Definitions;
+using GestResto.Logic.Services.Helpers;
+using NHibernate;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestResto.Logic.Services.NHibernate
+{
+    class NHibernateItemService : IItemService
+    {
+        private ISession session = NHibernateConnexion.OpenSession();
+
+        #region IItemService Membres
+
+        #endregion
+    }
+}
