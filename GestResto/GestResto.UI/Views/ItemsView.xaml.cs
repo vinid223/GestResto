414a5d70c0efc0cb97241f51e6ce93b9dae00629
Tommy Demers <tommy1416@hotmail.com>
2014-10-16 18:13:44 -0400

    Lire desc. Adapter ITEM et ORM
    Ajout champs EstActif dans la classe Item, Réussite d'un ajout d'item
    dans la BD. Modification de l'interface item, ajout des boutons de
    navigation, permet de voir la liste d'items dans la liste de boutons
    dans l'interface,

--- GestResto/GestResto.UI/Views/ItemsView.xaml.cs
+++ GestResto/GestResto.UI/Views/ItemsView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GestResto.Logic.Model.Entities;
+using GestResto.UI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,49 @@ namespace GestResto.UI.Views
     /// </summary>
     public partial class ItemsView : UserControl
     {
+        public ItemsViewModel ViewModelItem { get { return (ItemsViewModel)DataContext; } }
+        //public CategorieViewModel ViewModelCategorie { get { return (CategorieViewModel)DataContext; } }
+
+
+        public IList<Item> listeItems;
+       // public IList<Categorie> listeCategories;
+
         public ItemsView()
         {
             InitializeComponent();
+            DataContext = new ItemsViewModel();
+            // Création de la liste d'items
+            listeItems = ViewModelItem.ObtenirTousLesItems();
+            lbxListeCategorie.ItemsSource = listeItems;
+            // Création de la liste de catégories
+            //listeCategories = ViewModelCategorie.ObtenirToutesLesCategories();
+            //cboCategorieLiee.ItemsSource = listeCategories;
+
+        }
+        /// <summary>
+        /// Lorsque l'utilisateur clique sur le bouton d'un item dans la liste, 
+        /// pour afficher les informations de l'items dans les champs de l'écran.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnItem_Click(object sender, RoutedEventArgs e)
+        {
+            Item item = (Item)((sender as Button).CommandParameter);
+            ViewModelItem.Item = item;
+
+        }
+
+        /// <summary>
+        /// Lorsque l'utilisateur veut ajouter un item, on vide les champs.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAjouter_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            // Création d'un item temporaire.
+            Item itemTemp = new Item("Entrez le nom de l'item.", null, null, true);
+
+
         }
     }
 }
