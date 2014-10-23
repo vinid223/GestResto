20eaa9aa1573c81ffe51b1e8aca68b443fcaee5d
Vincent Desrosiers <vinid223@gmail.com>
2014-10-23 16:03:17 -0400

    Modif Nhibernate Employé

--- GestResto/GestResto.Logic/Services/NHibernate/NHibernateEmployeService.cs
+++ GestResto/GestResto.Logic/Services/NHibernate/NHibernateEmployeService.cs
@@ -14,14 +14,22 @@ namespace GestResto.Logic.Services.NHibernate
 {
     public class NHibernateEmployeService : IEmployeService
     {
+        // Définition des variables de session
         private ISession session = NHibernateConnexion.OpenSession();
         private ISession sessionLazy = NHibernateConnexion.OpenSession();
 
         #region IEmployeService Membres
 
+        /// <summary>
+        /// Focntion permettant de créer un employé
+        /// </summary>
+        /// <param name="employe">Prend un employé en paramètre</param>
         public void Create(Employe employe)
         {
+            // On réinitialise la session
             session = NHibernateConnexion.OpenSession();
+
+            // On effectue la transaction
             using (var transaction = session.BeginTransaction())
             {
                 session.Save(employe);
@@ -30,55 +38,98 @@ namespace GestResto.Logic.Services.NHibernate
             session.Close();
         }
 
+        /// <summary>
+        /// Fonction permettant de charger la liste des employés
+        /// </summary>
+        /// <returns>Retourne la liste de tous les employés</returns>
         public IList<Employe> RetriveAll()
         {
+            // On redéfinie la variable de session
             sessionLazy = NHibernateConnexion.OpenSession();
+
+            // On se définie une liste temporaire
             IList<Employe> listeTemp = sessionLazy.Query<Employe>().ToList();
 
             // On boucle dans tous nos résultat pour décrypter le mot de passe
+            // pour ne pas boucler dans la vue pour les mots de passe
             foreach (var employe in listeTemp)
             {
                 employe.MotDePasse = Employe.Decrypt(employe.MotDePasse);
             }
 
+            // On retourne la liste
             return listeTemp;
         }
 
+        /// <summary>
+        /// Fonction permettant de sélectionner un employé
+        /// </summary>
+        /// <param name="args">Liste des arguments permettant de sélectionner un employé</param>
+        /// <returns>Retourne l'employé recherché</returns>
         public Employe Retrive(RetrieveEmployeArgs args)
         {
+            // On redéfinie la variable de session
             session = NHibernateConnexion.OpenSession();
+
+            // On se crée un employé temporaire
             Employe employeTemp;
 
+            // On test si on vient avec un mot de passe et un numéro d'employé
+            // si c'est le cas c'est qu'on tente de se connecter
             if (args.MDP != null && args.NoEmploye != null)
             {
+                // On effectue la requête avec les paramètres
                 var result = from c in session.Query<Employe>()
                              where c.MotDePasse == args.MDP &&
                              c.NoEmploye == args.NoEmploye
                              select c;
+
+                // On enregistre le résultat dans notre employé temporaire
                 employeTemp = result.FirstOrDefault();
             }
             else
             {
+                // On effectue la requête avec seulement un ID puisqu'on n'est pas ici pour s'authentifier
                 var result = from c in session.Query<Employe>()
                              where c.IdEmploye == args.IIdEmploye
                              select c;
+
+                // On enregistre dans notre employé temporaire
                 employeTemp = result.FirstOrDefault();
             }
 
+            // On ferme la session et on retourne l'employé sélectionné
             session.Close();
             return employeTemp;
         }
 
+        /// <summary>
+        /// Fonction permettant de modifier un employé existant
+        /// </summary>
+        /// <param name="employe">Employé qu'on souhaite modifier</param>
         public void Update(Employe employe)
         {
+            // On enregistre le mot de passe non encrypté pour éviter d'encrypter 2 fois le même mot de passe
+            string AncienMDPNonEncrypte = employe.MotDePasse;
+
+            // On encrypte le mot de passe
             employe.MotDePasse = Employe.Encrypt(employe.MotDePasse);
+
+            // On réinitialise la variable de session
             session = NHibernateConnexion.OpenSession();
+
+            // On effectue la transaction
             using (var transaction = session.BeginTransaction())
             {
                 session.Update(employe);
                 transaction.Commit();
             }
+
+            // On ferme la session
             session.Close();
+
+            // On redéfinie le mot de passe à l'objet par le mot de passe non encrypté
+            employe.MotDePasse = AncienMDPNonEncrypte;
         }
 
         #endregion
