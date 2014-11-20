0b50f17d7238ea1290b09ff0ca14fb7037c1bcd2
Tommy Demers <tommy1416@hotmail.com>
2014-11-20 16:17:42 -0500

    Suppression client

--- GestResto/GestResto.Logic/Model/Entities/FormatItem.cs
+++ GestResto/GestResto.Logic/Model/Entities/FormatItem.cs
@@ -125,7 +125,7 @@ namespace GestResto.Logic.Model.Entities
 
         public override string ToString()
         {
-            return ItemAssocie.Nom + " " + FormatAssocie.Libelle + " " + Prix;
+            return ItemAssocie.Nom + " (" + FormatAssocie.Libelle + ") " + Prix;
         }
         /// <summary>
         /// Permet de comparer deux catégories.
